95be76ce8ea6348f64bf9ce2991161743048f13c
rfcsmoker <fingerflow@tutamail.com>
2025-09-23 21:57:12 +0300

    Add MatterId enum class

---
+++ include/world/matter_id.hpp
@@ -0,0 +1,15 @@
+#ifndef FORKFLOW_WORLD_MATTER_ID_HPP
+#define FORKFLOW_WORLD_MATTER_ID_HPP
+
+#include <cstdint>
+
+namespace forkflow::world {
+
+enum class MatterId: std::uint8_t {
+  NONE = 0,
+  UNIQUE = 255
+}; // enum class MatterId
+
+} // namespace forkflow::world
+
+#endif // FORKFLOW_WORLD_MATTER_ID_HPP
