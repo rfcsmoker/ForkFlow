410b8af344180e9d05ffa5254a8246ea6bd1ca2a
rfcsmoker <fingerflow@tutamail.com>
2025-09-25 08:28:25 +0300

    Add Matter basic class

---
+++ include/world/matter.hpp
@@ -0,0 +1,10 @@
+#ifndef FORKFLOW_WORLD_MATTER_HPP
+#define FORKFLOW_WORLD_MATTER_HPP
+
+namespace forkflow::world {
+
+class Matter {}; // class Matter
+
+} // namespace forkflow::world
+
+#endif // FORKFLOW_WORLD_MATTER_HPP
