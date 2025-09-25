0f6bc163733cd7bbf5f4a50d343bf1758906242f
rfcsmoker <fingerflow@tutamail.com>
2025-09-25 19:05:36 +0300

    Add Metrics world type

---
+++ include/world/metrics.hpp
@@ -0,0 +1,12 @@
+#ifndef FORKFLOW_WORLD_METRICS_HPP
+#define FORKFLOW_WORLD_METRICS_HPP
+
+#include <cstdint>
+
+namespace forkflow::world {
+
+using Metrics = std::uint16_t;
+
+} // namespace forkflow::world
+
+#endif // FORKFLOW_WORLD_METRICS_HPP
