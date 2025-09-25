94aa04cfae0fe065112765e821647a8725ad5605
rfcsmoker <fingerflow@tutamail.com>
2025-09-25 19:30:41 +0300

    Add include vector operations

--- include/math/vector.hpp
+++ include/math/vector.hpp
@@ -1,7 +1,11 @@
 #ifndef FORKFLOW_MATH_VECTOR_HPP
 #define FORKFLOW_MATH_VECTOR_HPP
 
+#include <cstddef>
+
+#include <boost/container_hash/hash.hpp>
 #include <boost/qvm/vec.hpp>
+#include <boost/qvm/vec_operations.hpp>
 
 namespace forkflow::math {
 
@@ -9,5 +13,19 @@ template <typename T>
 using Vector3D = boost::qvm::vec<T, 3>;
 
 } // namespace forkflow::math
+
+namespace std {
+
+template<typename T>
+struct hash<forkflow::math::Vector3D<T>> {
+  std::size_t operator()(const forkflow::math::Vector3D<T>& vec) const {
+    auto seed = std::size_t(0);
+    for (auto i = 0; i < 3; ++i)
+      boost::hash_combine(seed, vec.a[i]);
+    return seed;
+  }
+}; // struct hash<forkflow::math::Vector3D<T>>
+
+} // namespace std
 
 #endif // FORKFLOW_MATH_VECTOR_HPP
