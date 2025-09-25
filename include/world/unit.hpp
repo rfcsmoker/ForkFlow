df9a78e21fe0bcc49e5a65fcff9efa165d8c88a6
rfcsmoker <fingerflow@tutamail.com>
2025-09-25 08:32:19 +0300

    Add Unit class

---
+++ include/world/unit.hpp
@@ -0,0 +1,10 @@
+#ifndef FORKFLOW_WORLD_UNIT_HPP
+#define FORKFLOW_WORLD_UNIT_HPP
+
+namespace forkflow::world {
+
+class Unit {}; // class Unit
+
+} // namespace forkflow::world
+
+#endif // FORKFLOW_WORLD_UNIT_HPP
