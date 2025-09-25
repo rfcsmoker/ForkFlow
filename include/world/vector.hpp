2c216efc33339ee66b80e0f1bbf632a592cb4ac6
rfcsmoker <fingerflow@tutamail.com>
2025-09-25 19:16:25 +0300

    Add Vector3D world type

---
+++ include/world/vector.hpp
@@ -0,0 +1,13 @@
+#ifndef FORKFLOW_WORLD_VECTOR_HPP
+#define FORKFLOW_WORLD_VECTOR_HPP
+
+#include "metrics.hpp"
+#include "math/vector.hpp"
+
+namespace forkflow::world {
+
+using Vector3D = forkflow::math::Vector3D<Metrics>;
+
+} // namespace forkflow::world
+
+#endif // FORKFLOW_WORLD_VECTOR_HPP
