ef9587849bb97147c6694637278fd9cf33fa466a
rfcsmoker <fingerflow@tutamail.com>
2025-09-27 15:15:10 +0300

    Add general world Id type

---
+++ include/world/id.hpp
@@ -0,0 +1,12 @@
+#ifndef FORKFLOW_WORLD_ID_HPP
+#define FORKFLOW_WORLD_ID_HPP
+
+#include <cstdint>
+
+namespace forkflow::world {
+
+using Id = std::uint16_t;
+
+} // namespace forkflow::world
+
+#endif // FORKFLOW_WORLD_ID_HPP
