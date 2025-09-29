9890785df8fb85b85e78890eff82ae78958a9a5b
rfcsmoker <fingerflow@tutamail.com>
2025-09-29 12:08:40 +0300

    Add Avatar class

---
+++ include/world/avatar.hpp
@@ -0,0 +1,10 @@
+#ifndef FORKFLOW_WORLD_AVATAR_HPP
+#define FORKFLOW_WORLD_AVATAR_HPP
+
+namespace forkflow::world {
+
+class Avatar {}; // class Avatar
+
+} // namespace forkflow::world
+
+#endif // FORKFLOW_WORLD_AVATAR_HPP
